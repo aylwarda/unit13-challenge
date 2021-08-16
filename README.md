dafda76b3e609a119363c5ad9b83c3c8f1c18a80
AylwardA <drew.aylward@gmail.com>
2021-08-16 12:41:51 +1000

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # unit13-challenge (a.k.a. Project RoboAdvisor)
-This project repo has a strange name, but bear with me.  This is just a name, required for some Fintech homework I did.  What this Project is about, is AWS Lex and a Robo Advisor I built as part of the homework.
+This project repo has a strange name, but bear with me.  This is just a name, required for some Fintech homework I did.  What this Project is about, is AWS Lex and Lambda and a Robo Advisor I built as part of the homework.
+
+---
+## Intro
+This Project consists of a number of different components (see below).  One requires access to AWS Lex to test the RoboAdvisor Lex Bot as it has not been integrated into any accessible URL.
+
+There is however, a media file which shows an interaction.  It is short and while it doesn't show the myriad of different options possible, it gives a sense of what is possible.
+
+---
+## RoboAdvisor Bot
+### AWS Lex  
+The RoboAdvisor Bot is where all the magic happens.  The Bot requires a set of interactions, i.e. prompts to the bot and "canned" responses from the Bot to navigate the user toward their goal of "investing for their retirement".  
+
+The Bot then calls a lambda function which validates 'age', 'amount' to be invested and based on the chosen level of risk the user wants to take, suggests where to invest the amount entered (and at what percentages).
+
+### AWS Lambda  
+AWS Lambda is a service that enables the creation of services which can then be interacted with, from anywhere.  In this case, a Lambda Function was created which is no more than a python file which has within it, the set of funtions that allow for the integration into AWS Lex and our specific Bot, and other functions that validate the user input.
+
+### Files
+- `RoboAdvisor_Interaction.mp4` (in this repo in [`RoboAdvisor`](./RoboAdvisor) as a Zip file) shows the interaction.
+- Within the [`RoboAdvisor/Icons`](./RoboAdvisor/Icons) directory you will find some of the icons I used within the Bot (see Acknowledgements section for URI and license info).
+- Within the [`RoboAdvisor/LexExports`](../RoboAdvisor/LexExports) directory you will find `RoboAdvisor_Export.json` which defines the Bot, intent and slots, which can be imported into AWS Lex.
+- The `lambda_function.py` python file which AWS Lambda was based on, and found in [`RoboAdvisor`](./RoboAdvisor).
+
+---
+## Acknowledgements
+### Sources
+- The Bot was made possible by using the services of [AWS Lex](https://us-west-2.console.aws.amazon.com/lex/home?region=us-west-2)
+- The integration of the Bot with validation functions, was made possible by using the services of [AWS Lambda](https://us-west-2.console.aws.amazon.com/lambda/home?region=us-west-2)
+- All icons used within the AWS Lex RoboAdvisor where downloaded from [iconfinder.com](https://www.iconfinder.com/) for free
+
+### Licenses
+The Iconfinder icons are licenced under creative commons (attribution 3.0 unported; more on that [here](https://creativecommons.org/licenses/by/3.0/)).
--- RoboAdvisor/lambda_function.py
+++ RoboAdvisor/lambda_function.py
@@ -3,14 +3,28 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 ### Functionality Helper Functions ###
-def parse_int(n):
+def parse_val(n,to_type):
     """
-    Securely converts a non-integer value to integer.
+    Securely converts string value to integer or float.
     """
-    try:
-        return int(n)
-    except ValueError:
-        return float("nan")
+    # Test for the type, then convert
+    # else return NaN
+    if isinstance(n,str) and to_type == 'int':
+        try:
+            # convert the string to float before int
+            # to provide the best chance of success
+            return int(float(n))
+        except ValueError:
+            return float("NaN")
+    elif isinstance(n,str) and to_type == 'float':
+        try:
+            return float(n)
+        except ValueError:
+            # In the event that investment amount is not able to be
+            # converted to a float, then return 0 which will force
+            # another prompt for the correct value as opposed to
+            # accepting and "working around" an incorrect value
+            return float("0")
 
 
 def build_validation_result(is_valid, violated_slot, message_content):
@@ -25,6 +39,45 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "violatedSlot": violated_slot,
         "message": {"contentType": "PlainText", "content": message_content},
     }
+
+
+def validate_data(age, investment_amount, intent_request):
+    """
+    Validates the data provided by the user.
+    """
+
+    # Validate that the user:
+    #  - has been born!
+    #  - is less than the age of 65
+    if age is not None:   # if not null, validate
+        # Parameters coming through from the Bot (AWS Lex)
+        # will be strings, so it is important to cast them to float
+        age = parse_val(age,'int')
+        if age < 0 or age > 64:
+            return build_validation_result(
+                False,
+                "age",
+                "The maximum age to contract this service is 64, can you provide an age between 0 and 64 please?",
+            )
+
+    # Validate that investment_amount is > 0
+    if investment_amount is not None:  # if not null, validate
+        # Parameters coming through from the Bot (AWS Lex)
+        # will be strings, so it is important to cast them to float
+        #
+        # For this particular function, validating investment_amount
+        # is not required to provide a result back to the user
+        # but I'm doing it anyway :)
+        investment_amount = parse_val(investment_amount,'float')
+        if investment_amount < 5000 or not isinstance(investment_amount,float):
+            return build_validation_result(
+                False,
+                "investmentAmount",
+                "The minimum investment amount is $5,000 USD, could you please provide a greater amount?",
+            )
+
+    # A True result is returned if age or investment_amount are valid
+    return build_validation_result(True, None, None)
 
 
 ### Dialog Actions Helper Functions ###
@@ -86,6 +139,7 @@ def recommend_portfolio(intent_request):
     Performs dialog management and fulfillment for recommending a portfolio.
     """
 
+    # Get specific values for slot names
     first_name = get_slots(intent_request)["firstName"]
     age = get_slots(intent_request)["age"]
     investment_amount = get_slots(intent_request)["investmentAmount"]
@@ -97,20 +151,55 @@ def recommend_portfolio(intent_request):
         # Use the elicitSlot dialog action to re-prompt
         # for the first violation detected.
 
-        ### YOUR DATA VALIDATION CODE STARTS HERE ###
+        # Gets all the slots for processing later
+        slots = get_slots(intent_request)
 
-        ### YOUR DATA VALIDATION CODE ENDS HERE ###
+        # Validates user's input using validate_data()
+        validation_result = validate_data(age, investment_amount, intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
 
         # Fetch current session attibutes
         output_session_attributes = intent_request["sessionAttributes"]
 
         return delegate(output_session_attributes, get_slots(intent_request))
 
-    # Get the initial investment recommendation
+    ### If the data provided is valid, then set
+    #   the appropriate recommendation
 
-    ### YOUR FINAL INVESTMENT RECOMMENDATION CODE STARTS HERE ###
+    # Make sure we're working with predictable values
+    risk_level = risk_level.lower()
 
-    ### YOUR FINAL INVESTMENT RECOMMENDATION CODE ENDS HERE ###
+    # Determine the recommendation based on risk level
+    if risk_level == 'none' or risk_level == 'zero':
+        initial_recommendation = "100% bonds (AGG), 0% equities (SPY)"
+
+    elif risk_level == 'very low':
+        initial_recommendation = "80% bonds (AGG), 20% equities (SPY)"
+
+    elif risk_level == 'low':
+        initial_recommendation = "60% bonds (AGG), 40% equities (SPY)"
+
+    elif risk_level == 'medium':
+        initial_recommendation = "40% bonds (AGG), 60% equities (SPY)"
+
+    elif risk_level == 'high':
+        initial_recommendation = "20% bonds (AGG), 80% equities (SPY)"
+
+    elif risk_level == 'very high':
+        initial_recommendation = "0% bonds (AGG), 100% equities (SPY)"
 
     # Return a message with the initial recommendation based on the risk level.
     return close(
@@ -133,6 +222,7 @@ def dispatch(intent_request):
     Called when the user specifies an intent for this bot.
     """
 
+    # Get the name of the current intent
     intent_name = intent_request["currentIntent"]["name"]
 
     # Dispatch to bot's intent handlers
